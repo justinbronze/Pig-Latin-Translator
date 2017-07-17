8e1cf77c054f26915a33829ff86a8ddf6182a023
justinbronze <justinjbronzell@yahoo.com>
2017-07-17 00:08:21 -0500

    Add files via upload

--- PigLatin.py
+++ PigLatin.py
@@ -1,8 +1,37 @@
-print('Welcome to the Pig Latin Translator. Input your word below.')
-word = (input("Word to be translated: "))
-if len(word) > 0 and word.isalpha():
-    wordpl = (word[1:]) + (word[0].lower()) + ('ay')
-    wordpl = (wordpl[0].upper() + wordpl[1:])
-    print(wordpl + ' is ' + word + ' in Pig Latin.')
+print('Welcome to the Pig Latin Translator. Input your word or phrase below.')
+sent = (input("Word or Phrase to be translated: "))
+
+sentlist1 = sent.strip('.').strip('?').strip('!').split(' ')
+nonletter = 0
+for w in sentlist1:
+    if w.isalpha() == False:
+        nonletter +=1
+sentlist = sent.strip('.').split(' ')
+if len(sent) > 0 and (nonletter == 0): 
+    psent = ''
+    if len(sentlist) == 1:
+        for word in sentlist:
+            wordpl = (word[1].upper() + word[2:] + word[0].lower() + ('ay'))
+    if len(sentlist) == 2:
+        for word in sentlist:
+            if word == sentlist[0]:
+                wordpl = (word[1].upper() + word[2:] + word[0].lower() + ('ay') + ' ')
+            else:
+                wordpl = (word[1].upper() + word[2:] + word[0].lower() + ('ay'))
+            psent += wordpl
+    else:    
+        for word in sentlist:
+            if word == sentlist[0]:
+                if len(word) == 1:
+                    wordpl = (word[0].upper() + ('ay') + ' ')
+                else:
+                    wordpl = (word[1].upper() + word[2:] + word[0].lower() + ('ay') + ' ')
+            elif word == sentlist[-1]:
+                wordpl = (word[1:] + word[0].lower() + 'ay')
+            else:
+                wordpl = (word[1:]) + (word[0].lower()) + ('ay') + " "
+            psent += wordpl
+
+    print(psent + ' is ' + sent.strip('.') + ' in Pig Latin.')
 else:
-    print("You didn't input a word. Please try again.")
+    print("You didn't input a word or your submission included a number. Please try again.")
